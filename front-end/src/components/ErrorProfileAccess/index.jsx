d0622bbca29d6210778c0733f070d18405e91238
silvia brundu <silviabrundu13@gmail.com>
2024-02-06 22:47:10 +0100

    feat: error page if user is not logged in

---
+++ front-end/src/components/ErrorProfileAccess/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import './index.css';
+import Header from '../Header';
+import Footer from '../Footer';
+
+function ErrorProfileAccess() {
+    return (
+        <>
+            <Header />
+            <main className='main bg-dark'>
+                <div className='error_container_all'>
+                    <div className='error_container_title'>
+                        <h1 className='error_title'>404</h1>
+                    </div>
+                    <div className='error_container_text'>
+                        <p className='error_text'>Please log in to access this page</p>
+                    </div>
+                    <div className='error_container_link'>
+                        <Link to='/signin' className='error_link'>
+                            Return to the sign in page
+                        </Link>
+                        {/* Link remplace la balise a en react  */}
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default ErrorProfileAccess
--- front-end/src/pages/Profile/index.jsx
+++ front-end/src/pages/Profile/index.jsx
@@ -1,9 +1,21 @@
 import Account from '../../components/Account';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { useSelector } from "react-redux";
+import ErrorProfileAccess from "../../components/ErrorProfileAccess"
 import './index.css';
 
 function Profile() {
+
+    //j'affiche une page d'erreur si utilisateur pas connecté
+    const token = useSelector((state) => state.login.token);
+
+    if (!token) {
+        return (
+            <ErrorProfileAccess />
+        )
+    }
+
     return (
         <>
             <Header />
@@ -13,56 +25,56 @@ function Profile() {
                     <div className='edit-info'>
                         <label>User Name:</label>
                         <input
-                        type='text'
+                            type='text'
                         />
                     </div>
                     <div className='edit-info'>
                         <label>First Name:</label>
-                        <input 
-                        type='text'
-                        // disabled désactive le champ du formulaire
-                        disabled 
+                        <input
+                            type='text'
+                            // disabled désactive le champ du formulaire
+                            disabled
                         />
                     </div>
                     <div className='edit-info'>
                         <label>Last Name:</label>
-                        <input 
-                        type='text' 
-                        disabled 
+                        <input
+                            type='text'
+                            disabled
                         />
                     </div>
                     <div className='edit-all-buttons'>
                         <button className='edit-button' type='submit'>
-                        Save
+                            Save
                         </button>
-                        <button className='edit-button' type='button' 
+                        <button className='edit-button' type='button'
                         >
-                        Cancel
+                            Cancel
                         </button>
                     </div>
                 </form>
                 <main className='main bg-dark'>
                     <div className='greeting-content'>
-                        <h1 className='greeting'> 
-                            Welcome back <br />... 
+                        <h1 className='greeting'>
+                            Welcome back <br />...
                         </h1>
                         { /* devoir probablement crée un composant qui affichera nom et prenom */}
                         <button className='edit-button'>
                             Edit Name
                         </button>
-                    </div> 
+                    </div>
                     <Account
                         title='Argent Bank Checking (x8349)'
                         amount='$2,082.79'
-                        description='Available Balance'/>
+                        description='Available Balance' />
                     <Account
                         title='Argent Bank Savings (x6712)'
                         amount='$10,928.42'
-                        description='Available Balance'/>
+                        description='Available Balance' />
                     <Account
                         title='Argent Bank Credit Card (x8349)'
                         amount='$184.30'
-                        description='Current Balance'/>
+                        description='Current Balance' />
                 </main>
             </div>
             <Footer />
